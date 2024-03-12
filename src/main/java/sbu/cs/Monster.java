824abf7be303160441af07d0fabbe71c46263d14
Shayan Shahrabi <shayanshahrab@gmail.com>
2024-03-12 08:58:29 -0700

    update file: Monster.java | implemented clean code

--- src/main/java/sbu/cs/Monster.java
+++ src/main/java/sbu/cs/Monster.java
@@ -1,29 +1,26 @@
 package sbu.cs;
 
-public abstract class Monster implements GameObject
-{
+public abstract class Monster implements GameObject {
     public String name;
     protected int health;
     protected int attackPower;
     protected  boolean isAlive;
-
-    public Monster(String name)
-    {
+    //-----------------------------------------------------------------------------
+    // constructor
+    public Monster(String name){
         this.name = name;
         attackPower = 10;
         health = 100;
         isAlive = true;
     }
-
-    public void attack(GameObject target)
-    {
-        System.out.println(name + " attacked " + ((Player)target).playerName);
+    //-----------------------------------------------------------------------------
+    public void attack(GameObject target){
+        System.out.println(name + " ATTACKED " + ((Player)target).playerName);
         target.takeDamage(attackPower);
     }
-
-    public void takeDamage(int damage)
-    {
-        System.out.println(name + " Took " + damage + " Damage!");
+    //-----------------------------------------------------------------------------
+    public void takeDamage(int damage){
+        System.out.println(name + " TOOK " + damage + " DAMAGE!");
         health -= damage;
         if(health <= 0)
         {
@@ -31,12 +28,12 @@ public abstract class Monster implements GameObject
             die();
             return;
         }
-        System.out.println(name + " health is " + health);
+        System.out.println(name + " HEALTH IS " + health);
     }
-
-    public void die()
-    {
+    //-----------------------------------------------------------------------------
+    public void die(){
         isAlive = false;
         System.out.println(name  + " DIED");
     }
+    //-----------------------------------------------------------------------------
 }
