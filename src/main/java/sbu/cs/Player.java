6bc553c9b3481a69a5a1f2e369bf2840d4cccc21
Shayan Shahrabi <shayanshahrab@gmail.com>
2024-03-12 08:59:43 -0700

    update file: Player.java | implemented clean code

--- src/main/java/sbu/cs/Player.java
+++ src/main/java/sbu/cs/Player.java
@@ -1,42 +1,38 @@
 package sbu.cs;
 
-public abstract class Player implements GameObject
-{
+public abstract class Player implements GameObject{
     public int health;
     protected boolean isAlive ;
     protected int attackPower;
     public String playerName;
-
-    public Player(String playerName)
-    {
+    //-----------------------------------------------------------------------------
+    // constructor
+    public Player(String playerName){
         isAlive = true;
         health = 100;
         this.playerName = playerName;
         attackPower = 10;
     }
-
-    public void attack(GameObject target)
-    {
+    //-----------------------------------------------------------------------------
+    public void attack(GameObject target){
         System.out.println(playerName + " attacked " + ((Monster)target).name + "! ");
         target.takeDamage(attackPower);
     }
-
-    public void takeDamage(int damage)
-    {
+    //-----------------------------------------------------------------------------
+    public void takeDamage(int damage){
         System.out.println(playerName + " Took " + damage + " Damage!");
         health -= damage;
-        if(health <= 0)
-        {
+        if(health <= 0){
             health = 0;
             die();
             return;
         }
         System.out.println(playerName + " health is " + health);
     }
-
-    public void die()
-    {
+    //-----------------------------------------------------------------------------
+    public void die(){
         isAlive = false;
         System.out.println(playerName + " DIED");
     }
+    //-----------------------------------------------------------------------------
 }
